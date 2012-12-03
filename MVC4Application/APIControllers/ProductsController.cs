a98bd27b4d77522bac5ad828585c912645b38981
cleancodenz2 <cleancodenz2@cleancode.co.nz>
2012-12-03 15:32:37 +1300

    removed old product list

--- MVC4Application/APIControllers/ProductsController.cs
+++ MVC4Application/APIControllers/ProductsController.cs
@@ -13,14 +13,7 @@ namespace MVC4Application.APIControllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[] 
-        { 
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
-        };
-
-
+       
         private readonly IProductService _productService;
 
         public ProductsController(IProductService productService)
